a17fe3364352b8023c19c3d9208abb94a187b25a
Rohan Babbar <rohanbabbar2003@gmail.com>
2025-08-29 00:16:29 +0530

    updated

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ import tripRoutes from "./src/routes/tripRoutes.js";
 import hotelRoutes from "./src/routes/hotelRoutes.js";
 import attractionRoutes from "./src/routes/attractionRoutes.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
-import chatRoutes from "./src/routes/chatRoutes.js"
+import chatRoutes from "./src/routes/chatRoutes.js";
 
 const app = express();
 
 const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
 const corsOptions = {
-        origin: allowedOrigin,
-        optionsSuccessStatus: 200 
-    };
+  origin: allowedOrigin,
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -26,8 +26,7 @@ app.use(morgan("dev"));
 app.use("/api/trips", tripRoutes);
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/attractions", attractionRoutes);
-app.use("/api/assist", chatRoutes);          
-
+app.use("/api/assist", chatRoutes);
 
 app.use(errorHandler);
 
